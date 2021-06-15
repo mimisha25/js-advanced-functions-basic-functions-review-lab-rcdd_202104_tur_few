47ed2d52ddb050cc9edbe56592125ca78f119552
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-15 19:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,23 +2,19 @@
 function saturdayFun(activity="roller-skate"){
    return `This Saturday, I want to ${activity}!`;
  }
-
  const mondayWork = function (activity = "go to the office"){
    return `This Monday, I will ${activity}.`;
  };
-
  function wrapAdjective(flair = "*"){
    return function(adjective="special"){
      return `You are ${flair}${adjective}${flair}!`;
    }
  }
-
  const Calculator ={};
  Calculator["add"] = function(n1, n2){return n1+n2};
  Calculator["subtract"] = function(n1, n2){return n1-n2};
  Calculator["multiply"] = function(n1, n2){return n1*n2};
  Calculator["divide"] = function(n1, n2){return n1/n2};
-
  function actionApplyer(start, array){
    if(array.length === 0){
      return start;
